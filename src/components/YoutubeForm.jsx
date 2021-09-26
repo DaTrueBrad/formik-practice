d7d0467a39e48cdef3f6dc8b7c624a3ca72ef662
Brady Bott <bottbrady@gmail.com>
2021-09-25 22:27:29 -0600

    practicing formik

---
+++ src/components/YoutubeForm.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useFormik } from "formik";
+
+function YoutubeForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit: (values) => {
+      console.log("Form Data:", formik.values);
+    },
+  });
+
+  // console.log("Form Values:", formik.values)
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <label htmlFor="name">Channel</label>
+      <input
+        type="text"
+        id="channel"
+        name="channel"
+        onChange={formik.handleChange}
+        value={formik.values.channel}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default YoutubeForm;
